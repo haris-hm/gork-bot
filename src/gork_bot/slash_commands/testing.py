2276667d1029a6583c779bd6e76f94bf1e1ce7dd
Haris Mehuljic <haris.mehuljic@drake.edu>
2026-07-26 21:19:29 -0500

    Set up Node project

--- src/gork_bot/slash_commands/testing.py
+++
@@ -1,9 +0,0 @@
-from discord import Interaction
-
-from gork_bot.bot import GorkBot
-
-
-def setup(bot: GorkBot) -> None:
-    @bot.tree.command(name="echo", description="Echos the provided message.")
-    async def __echo_command(interaction: Interaction, message: str):
-        await interaction.response.send_message(message)
